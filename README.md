38d587464a0e2b686bd8dc29c5be0c12c5814536
Anupam Gogoi <c059237@yara.com>
2023-10-26 18:05:42 -0300

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+### Command
+
+```
+go build -gcflags="all=-N -l" -v --buildmode=plugin -o plugin/go-sonic-kafka-connector.so pkg/listener/KafkaListener.go
+```
---
+++ pkg/listener/KafkaListener.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/IBM/sarama"
+	"log"
+	"os"
+)
+
+type KafkaListener struct {
+}
+
+func (l *KafkaListener) Listen() string {
+	confg := sarama.NewConfig()
+	brokers := []string{"localhost:29092"}
+	consumer, err := sarama.NewConsumer(brokers, confg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("********** Kafka Consumer started **********")
+	pc, err := consumer.ConsumePartition("DATA.TOPIC", 0, sarama.OffsetOldest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	stop := make(chan os.Signal, 1)
+	run := true
+	for run {
+		select {
+		case msg := <-pc.Messages():
+			log.Println(msg.Value)
+		case err := <-pc.Errors():
+			log.Fatal(err)
+		case <-stop:
+			run = false
+		}
+	}
+	return ""
+}
+
+var EXPORT_KAFKA_LISTENER KafkaListener
